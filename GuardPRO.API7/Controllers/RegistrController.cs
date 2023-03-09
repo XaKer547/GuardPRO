60006eaf296ae4b4ba4c8b7848e78f2ddcbf06b7
Popka <Popka@DESKTOP-NR8O712>
2023-03-09 20:04:28 +0300

    refactoring

--- GuardPRO.API7/Controllers/RegistrController.cs
+++ GuardPRO.API7/Controllers/RegistrController.cs
@@ -4,17 +4,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GuardPRO.API7.Controllers;
-
-public class RegisterViewModel
-{
-    public bool IsSessuful { get; set; }
-    public string ErrorMessage { get; set; }
-}
 
 public class RegistrController : Controller
 {
@@ -27,44 +23,71 @@ public class RegistrController : Controller
 
     public IActionResult Index()
     {
-        return View(new RegisterViewModel { IsSessuful = tru});
+        return View();
     }
 
     [HttpPost]
-    public async Task<IActionResult> Index([FromForm] RegistrFormData formData)
+    public async Task<IActionResult> Index([FromForm] RegisterViewModel model)
     {
-        var user = await _context.Users.FirstOrDefaultAsync(x => x.Login == formData.UserLogin);
-        if (user != null)
-            return View(new RegisterViewModel { IsSessuful = false, ErrorMessage = "Такой пользователь уже существует"});
+        if (!ModelState.IsValid)
+            return View(model);
+
+        var userExists = await _context.Users.AnyAsync(x => x.Login == model.UserLogin);
+        if (userExists)
+        {
+            ModelState.AddModelError(nameof(model.UserLogin), "Такой логин уже занят!");
+            return View(model);
+        }
 
         using var hasher = MD5.Create();
-        var hash = hasher.ComputeHash(Encoding.UTF8.GetBytes(formData.UserPassword));
+        var hash = hasher.ComputeHash(Encoding.UTF8.GetBytes(model.UserPassword));
 
         await _context.Users.AddAsync(new User
         {
             Sername = "",
-            Name = formData.UserName,
+            Name = model.UserName,
             Patronic = "",
             Telephone = "",
-            DateOfBird = formData.UserBird,
+            DateOfBird = model.UserBird,
             SerialPassport = "",
             NumberPassport = "",
-            Login = formData.UserLogin,
-            Email = formData.UserEmail,
+            Login = model.UserLogin,
+            Email = model.UserEmail,
             Password = Convert.ToHexString(hash)
         });
 
         await _context.SaveChangesAsync();
 
-        return View(new RegisterViewModel { IsSessuful = true });
+        return RedirectToAction(nameof(SuccessRegistration));
+    }
+
+    public IActionResult SuccessRegistration()
+    {
+        return View();
     }
 }
 
-public class RegistrFormData
+public class RegisterViewModel
 {
+    [Display(Name = "Email")]
+    [Required]
+    [EmailAddress]
     public string UserEmail { get; set; }
+
+    [Display(Name = "Логин")]
+    [Required(ErrorMessage = "Требуется указать логин")]
     public string UserLogin { get; set; }
+
+    [Display(Name = "Пароль")]
+    [Required(ErrorMessage = "Требуется указать пароль")]
+    [PasswordPropertyText]
     public string UserPassword { get; set; }
+
+    [Display(Name = "Дата рождения")]
+    [Required(ErrorMessage = "Требуется указать дату рождения")]
     public DateTime UserBird { get; set; }
+
+    [Display(Name = "Имя пользователя")]
+    [Required(ErrorMessage = "Требуется указать имя пользователя")]
     public string UserName { get; set; }
 }
